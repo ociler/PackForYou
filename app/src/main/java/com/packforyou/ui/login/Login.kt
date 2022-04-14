07b0021257d10ddc8732aad2a06138fa8e4c76c2
Víctor Soriano López <victorsoriano_lafont@hotmail.es>
2022-04-14 12:52:20 +0200

    Dependency injection with Hilt implemented

--- app/src/main/java/com/packforyou/ui/login/Login.kt
+++ app/src/main/java/com/packforyou/ui/login/Login.kt
@@ -1,26 +1,23 @@
 package com.packforyou.ui.login
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.currentCompositionLocalContext
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.packforyou.data.dataSources.FirebaseDatabase
 import com.packforyou.data.models.DeliveryMan
 import com.packforyou.data.repositories.FirebaseCallback
 import com.packforyou.data.repositories.Response
 
-
-private lateinit var viewModel: LoginViewModel
 private lateinit var deliveryMen: List<DeliveryMan>
 
 @Composable
-fun Login() {
-    //viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+fun Login(
+    loginViewModel: LoginViewModel = viewModel()
+) {
 
 }
 
-private fun getAllDeliveryMan(): List<DeliveryMan> {
-    FirebaseDatabase.createRandomUser()
-
+private fun getAllDeliveryMan(viewModel: LoginViewModel): List<DeliveryMan> {
+    //FirebaseDatabase.createRandomUser()
     viewModel.getResponseUsingCallback(object : FirebaseCallback {
         override fun onResponse(response: Response) {
             deliveryMen = response.deliveryMen!!
--- app/src/main/java/com/packforyou/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/packforyou/ui/login/LoginViewModel.kt
@@ -3,8 +3,11 @@ package com.packforyou.ui.login
 import androidx.lifecycle.ViewModel
 import com.packforyou.data.repositories.FirebaseCallback
 import com.packforyou.data.repositories.LoginRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class LoginViewModel(
+@HiltViewModel
+class LoginViewModel @Inject constructor(
     private val repository: LoginRepository = LoginRepository()
 ) : ViewModel() {
     fun getResponseUsingCallback(callback: FirebaseCallback) =
