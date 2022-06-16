de71062ca27d0a13731ba9e418eafcd4c648321e
Víctor <victorsoriano_lafont@hotmail.es>
2022-06-16 17:46:05 +0200

    Update DirectionsApiService.kt

--- app/src/main/java/com/packforyou/api/DirectionsApiService.kt
+++ app/src/main/java/com/packforyou/api/DirectionsApiService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 //https://maps.googleapis.com/maps/api/directions/json?origin=1337%20India%20St%2C%20San%20Diego%2C%20CA%2092101%2C%20United%20States&destination=1485%20E%20St%2C%20San%20Diego%2C%20CA%2092101%2C%20United%20States
-// &waypoints=optimize:true|1200%20Third%20Ave%2C%20San%20Diego%2C%20CA%2092101%2C%20United%20States%7C707%20Tenth%20Ave%2C%20San%20Diego%2C%20CA%2092101%2C%20United%20States%7C180%20Broadway%20Suite%20101%2C%20San%20Diego%2C%20CA%2092101%2C%20United%20States%7C945%20Broadway%2C%20San%20Diego%2C%20CA%2092101%2C%20United%20States&key=AIzaSyD6MjIoMCFFaVDpUWToKwjfAaOxUQmFoHM
+// &waypoints=optimize:true|1200%20Third%20Ave%2C%20San%20Diego%2C%20CA%2092101%2C%20United%20States%7C707%20Tenth%20Ave%2C%20San%20Diego%2C%20CA%2092101%2C%20United%20States%7C180%20Broadway%20Suite%20101%2C%20San%20Diego%2C%20CA%2092101%2C%20United%20States%7C945%20Broadway%2C%20San%20Diego%2C%20CA%2092101%2C%20United%20States&key=
 
 //%7C is |
 
