9f71e43c23823118b2f1ec9efaf34db4d9d0257b
Víctor Soriano López <victorsoriano_lafont@hotmail.es>
2022-08-04 02:27:06 +0200

    algorithms ui implemented

--- app/src/main/java/com/packforyou/ui/packages/Packages.kt
+++ app/src/main/java/com/packforyou/ui/packages/Packages.kt
@@ -1,11 +1,15 @@
-package com.packforyou.ui.packages;
+package com.packforyou.ui.packages
 
 import android.content.Context
 import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
@@ -13,10 +17,12 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
@@ -37,6 +43,15 @@ import com.packforyou.ui.login.CurrentSession
 import com.packforyou.ui.theme.*
 
 lateinit var expanded: MutableState<Boolean>
+lateinit var selectedAlgorithm: MutableState<Algorithm>
+
+val algorithmOptions = listOf(
+    "Directions API",
+    "Brute Force",
+    "Closest Neighbour",
+    "Urgency",
+    "Custom Sort"
+)
 
 @Composable
 fun PackagesScreen(
@@ -47,6 +62,10 @@ fun PackagesScreen(
 ) {
 
     IsLoading.state = remember { mutableStateOf(false) }
+    expanded = remember { mutableStateOf(false) }
+    selectedAlgorithm = remember {
+        mutableStateOf(CurrentSession.algorithm)
+    }
 
     Column(Modifier.fillMaxHeight(.9f)) {
 
@@ -73,7 +92,78 @@ fun PackagesScreen(
             ) {
 
                 item {
-                    FilterButton(packagesViewModel, lifecycleOwner)
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "Ordered by: ",
+                            style = PackForYouTypography.labelMedium,
+                            fontWeight = FontWeight.SemiBold
+                        )
+
+                        Spacer(Modifier.weight(1f))
+
+                        if (!expanded.value) {
+                            EnabledAlgorithmItem(
+                                algorithmName = getStringGivenAlgorithm(
+                                    CurrentSession.algorithm
+                                ),
+                                isClickable = false
+                            )
+
+                            Spacer(modifier = Modifier.width(5.dp))
+                        }
+
+                        if (expanded.value) {
+                            Spacer(modifier = Modifier.width(10.dp))
+                            UpsideDownTriangle(
+                                modifier = Modifier
+                                    .size(32.dp)
+                                    .padding(end = 10.dp)
+                            )
+                        } else {
+                            Spacer(modifier = Modifier.width(5.dp))
+                            Column {
+                                Spacer(modifier = Modifier.height(10.dp))
+                                SidedTriangle(
+                                    modifier = Modifier
+                                        .size(32.dp)
+                                        .rotate(90f)
+                                        .padding(end = 10.dp)
+                                )
+                            }
+
+                            Spacer(modifier = Modifier.width(5.dp))
+                        }
+                        FilterButton()
+                        Spacer(Modifier.padding(end = 5.dp))
+                    }
+
+                    Spacer(Modifier.padding(bottom = 20.dp))
+                }
+
+                if (expanded.value) {
+                    item {
+                        LazyRow { //TODO fix enabledAlgorithmItem a bit lower bug
+                            items(algorithmOptions) { algorithmName ->
+                                if (getAlgorithmGivenString(algorithmName) == CurrentSession.algorithm) {
+                                    EnabledAlgorithmItem(
+                                        algorithmName = algorithmName
+                                    )
+                                } else {
+                                    DisabledAlgorithmItem(
+                                        algorithmName = algorithmName,
+                                        viewModel = packagesViewModel,
+                                        lifecycleOwner = lifecycleOwner
+                                    )
+                                }
+
+                                Spacer(modifier = Modifier.width(15.dp))
+                            }
+                        }
+
+                        Spacer(modifier = Modifier.height(25.dp))
+                    }
                 }
 
                 //we need the key value because if we don't use it, the column will recognize each
@@ -179,7 +269,9 @@ fun PackagesScreen(
                     }
                 },
                 textAlign = TextAlign.Center,
-                modifier = Modifier.fillMaxWidth().padding(vertical = 5.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 5.dp)
             )
         }
         StartRouteRectangularButton(navController, packages.value.isEmpty())
@@ -187,23 +279,156 @@ fun PackagesScreen(
 }
 
 @Composable
-fun FilterButton(viewModel: IPackagesViewModel, lifecycleOwner: LifecycleOwner) {
+fun UpsideDownTriangle(
+    modifier: Modifier = Modifier
+) {
+    Canvas(
+        modifier = modifier.aspectRatio(1f)
+    ) {
+        val rect = Rect(Offset.Zero, size)
+        val trianglePath = Path().apply {
+            moveTo(rect.bottomCenter.x, rect.bottomCenter.y)
+            lineTo(rect.topRight.x, rect.topRight.y)
+            lineTo(rect.topLeft.x, rect.topLeft.y)
+            close()
+        }
 
-    expanded = remember { mutableStateOf(false) }
-    var selectedAlgorithm by remember { mutableStateOf(Algorithm.NOT_ALGORITHM) }
+        drawIntoCanvas { canvas ->
+            canvas.drawOutline(
+                outline = Outline.Generic(trianglePath),
+                paint = Paint().apply {
+                    color = Black
+                    pathEffect = PathEffect.cornerPathEffect(rect.maxDimension / 4)
+                }
+            )
+        }
+    }
+}
 
+@Composable
+fun SidedTriangle(
+    modifier: Modifier = Modifier
+) {
+    Canvas(
+        modifier = modifier
+            .aspectRatio(1f)
+            .rotate(270f)
+    ) {
+        val rect = Rect(Offset.Zero, size)
+        val trianglePath = Path().apply {
+            moveTo(rect.centerLeft.x, rect.centerLeft.y)
+            lineTo(rect.topRight.x, rect.topRight.y)
+            lineTo(rect.bottomRight.x, rect.bottomRight.y)
+            close()
+        }
+
+        drawIntoCanvas { canvas ->
+            canvas.drawOutline(
+                outline = Outline.Generic(trianglePath),
+                paint = Paint().apply {
+                    color = Black
+                    pathEffect = PathEffect.cornerPathEffect(rect.maxDimension / 4)
+                }
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EnabledAlgorithmItem(
+    algorithmName: String,
+    isClickable: Boolean = true
+) {
     val context = LocalContext.current
 
-    val algorithmOptions = listOf(
-        "Directions API",
-        "Brute Force",
-        "Closest Neighbour",
-        "Urgency",
-        "Custom Sort"
-    )
+    Surface(
+        shape = RoundedCornerShape(20.dp),
+        color = Black,
+        onClick = {
+            if (isClickable) {
+                Toast
+                    .makeText(
+                        context,
+                        "You are already sorting the packages by $algorithmName",
+                        Toast.LENGTH_SHORT
+                    )
+                    .show()
 
-    Row(Modifier.padding(end = 5.dp, bottom = 25.dp)) {
-        Spacer(Modifier.weight(1f))
+                expanded.value = false
+            }
+        }
+    )
+    {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(vertical = 5.dp, horizontal = 10.dp)
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = algorithmName,
+                    color = Color.White,
+                    style = PackForYouTypography.labelMedium,
+                    fontWeight = FontWeight.SemiBold
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun DisabledAlgorithmItem(
+    algorithmName: String,
+    viewModel: IPackagesViewModel,
+    lifecycleOwner: LifecycleOwner
+) {
+    val context = LocalContext.current
+
+    Surface(
+        shape = RoundedCornerShape(20.dp),
+        color = White,
+        border = BorderStroke(width = 2.dp, color = Black)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .clickable {
+                    selectedAlgorithm.value = getAlgorithmGivenString(algorithmName)
+                    IsLoading.state.value = true
+
+                    computeProperAlgorithmAndUpdateRoute(
+                        selectedAlgorithm.value,
+                        viewModel,
+                        lifecycleOwner,
+                        context
+                    )
+
+                    expanded.value = false
+                }
+                .padding(vertical = 5.dp, horizontal = 10.dp)
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = algorithmName,
+                    color = Black,
+                    style = PackForYouTypography.labelMedium,
+                    fontWeight = FontWeight.SemiBold
+                )
+            }
+        }
+    }
+}
+
+
+@Composable
+fun FilterButton() {
+
+    Row {
         Box(
             modifier = Modifier
                 .clip(RoundedCornerShape(10.dp))
@@ -213,7 +438,7 @@ fun FilterButton(viewModel: IPackagesViewModel, lifecycleOwner: LifecycleOwner) 
         ) {
             IconButton(
                 onClick = {
-                    expanded.value = true
+                    expanded.value = !expanded.value
                 },
                 modifier = Modifier.size(32.dp)
             ) {
@@ -223,38 +448,6 @@ fun FilterButton(viewModel: IPackagesViewModel, lifecycleOwner: LifecycleOwner) 
                     tint = White,
                     modifier = Modifier.fillMaxSize()
                 )
-            }
-
-            DropdownMenu(
-                expanded = expanded.value,
-                onDismissRequest = { expanded.value = false }
-            ) {
-                algorithmOptions.forEach {
-                    DropdownMenuItem(
-                        onClick = {
-                            selectedAlgorithm = getAlgorithmGivenString(it)
-                            if (selectedAlgorithm != CurrentSession.algorithm) {
-                                IsLoading.state.value = true
-
-                                computeProperAlgorithmAndUpdateRoute(
-                                    selectedAlgorithm,
-                                    viewModel,
-                                    lifecycleOwner,
-                                    context
-                                )
-                            } else {
-                                Toast.makeText(
-                                    context,
-                                    "You are already sorting the packages by $it",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            }
-                            expanded.value = false
-
-                        }, text = {
-                            Text(text = it)
-                        })
-                }
             }
         }
     }
@@ -323,6 +516,30 @@ private fun getAlgorithmGivenString(algorithmString: String): Algorithm {
 
         else -> {
             Algorithm.NOT_ALGORITHM
+        }
+    }
+}
+
+private fun getStringGivenAlgorithm(algorithm: Algorithm): String {
+    return when (algorithm) {
+        Algorithm.DIRECTIONS_API -> {
+            "Directions API"
+        }
+
+        Algorithm.BRUTE_FORCE -> {
+            "Brute Force"
+        }
+
+        Algorithm.CLOSEST_NEIGHBOUR -> {
+            "Closest Neighbour"
+        }
+
+        Algorithm.URGENCY -> {
+            "Urgency"
+        }
+
+        else -> {
+            "Custom Algorithm"
         }
     }
 }
@@ -564,7 +781,7 @@ private fun computeProperAlgorithmAndUpdateRoute(
                 .observe(owner) { optimizedVeryUrgentRoute ->
 
                     //TODO SOLVE THIS LIVEDATA THING. The observe is triggered twice: One at the beginning
-                    //bc optimizedVeryUrgentRoute already has a value and anotherone
+                    //bc optimizedVeryUrgentRoute already has a value and another one
                     //when it should be triggered (when this object.value changes).
                     //This means that these lines of code are computed twice
                     val optimizedPackages = mutableListOf<Package>()
